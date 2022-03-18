f1388095546270755e4af1c1001d67fdeca66de6
VenomSolo <karolikjakub@gmail.com>
2022-03-18 05:43:48 +0100

    Additional cleanup and added types

--- src/features/components/Filters.tsx
+++ src/features/components/Filters.tsx
@@ -13,24 +13,35 @@ export function Filters() {
         <div className={styles.filters}>
             <Stack spacing={2} direction="row" justifyContent="space-around">
                 <TextField fullWidth
-                    id="outlined-basic"
-                    label="Name"
+                    id="NameInput"
                     variant="outlined"
                     value={useAppSelector(selectName)}
                     onChange={(e) => dispatch(changeName(e.target.value))} />
                 <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                    <InputLabel id="TypeInput">Type</InputLabel>
                     <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
                         value={useAppSelector(selectType)}
-                        label="Age"
                         onChange={(e) => dispatch(changeType(e.target.value))}
                     >
                         <MenuItem value={'all'}>All</MenuItem>
-                        <MenuItem value={'flying'}>Flying</MenuItem>
+                        <MenuItem value={'normal'}>Normal</MenuItem>
                         <MenuItem value={'fighting'}>Fighting</MenuItem>
+                        <MenuItem value={'flying'}>Flying</MenuItem>
+                        <MenuItem value={'poison'}>Poison</MenuItem>
+                        <MenuItem value={'ground'}>Ground</MenuItem>
+                        <MenuItem value={'rock'}>Rock</MenuItem>
+                        <MenuItem value={'bug'}>Bug</MenuItem>
+                        <MenuItem value={'ghost'}>Ghost</MenuItem>
+                        <MenuItem value={'steel'}>Steel</MenuItem>
                         <MenuItem value={'fire'}>Fire</MenuItem>
+                        <MenuItem value={'water'}>Water</MenuItem>
+                        <MenuItem value={'grass'}>Grass</MenuItem>
+                        <MenuItem value={'electric'}>Electric</MenuItem>
+                        <MenuItem value={'psychic'}>Psychic</MenuItem>
+                        <MenuItem value={'ice'}>Ice</MenuItem>
+                        <MenuItem value={'dragon'}>Dragon</MenuItem>
+                        <MenuItem value={'dark'}>Dark</MenuItem>
+                        <MenuItem value={'fairy'}>Fairy</MenuItem>
                     </Select>
                 </FormControl>
                 <Button color="primary" variant="contained"
